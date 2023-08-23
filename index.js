34bffde80edaab639e900bf3cf7b92870217f19d
ymotahhar <ymotahhars@gmail.com>
2023-08-23 19:19:00 +0330

    tt

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("click").addEventListener=("click",onclick)
+function onclick(){
+    document.getElementById("click").style.width.height="100%"
+    // document.getElementById("click").style.height="100%"
+
+}
+// document.getElementById("click").addEventListener=("click",unclick)
+// function onclick(){
+//     document.getElementById("click").style.width=100%
+
+// }
